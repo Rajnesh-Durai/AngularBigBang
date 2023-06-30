c9e66d0e9a296ad979d6c4d9e7f0f5ab23c5c1d3
Rajnesh Durai R T <127297822+Rajnesh-kanini@users.noreply.github.com>
2023-06-30 16:43:26 +0530

    Add files via upload

---
+++ BackEnd/Controller/UserController.cs
@@ -0,0 +1,46 @@
+﻿using BigBangAngular30thJune.Models;
+using BigBangAngular30thJune.Repository.DoctorRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BigBangAngular30thJune.Controllers
+{
+    [Route("PatientView")]
+    [ApiController]
+    public class UserController:ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [Authorize(Roles ="Patient")]
+        [HttpGet("List Of Doctors")]
+        public async Task<ActionResult<List<DoctorDetails>>> GetAllDoctors()
+        {
+            try
+            {
+                var item = await _userService.GetAllDoctors();
+                return Ok(item);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+        [Authorize(Roles = "Patient")]
+        [HttpPost("Booking an Appointmant")]
+        public async Task<ActionResult<Appointment>> BookAppointment(Appointment appointment)
+        {
+            try
+            {
+                var item = await _userService.BookAppointment(appointment);
+                return Ok(item);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
