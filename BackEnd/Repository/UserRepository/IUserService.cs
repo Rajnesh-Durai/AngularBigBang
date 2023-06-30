8083d298fb2bcd7d13ae7b2082f0d00e2e4a4981
Rajnesh Durai R T <127297822+Rajnesh-kanini@users.noreply.github.com>
2023-06-30 16:44:11 +0530

    Add files via upload

---
+++ BackEnd/Repository/UserRepository/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using BigBangAngular30thJune.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BigBangAngular30thJune.Repository.DoctorRepository
+{
+    public interface IUserService
+    {
+        public Task<List<DoctorDetails>> GetAllDoctors();
+        public Task<Appointment> BookAppointment(Appointment appointment);
+    }
+}
