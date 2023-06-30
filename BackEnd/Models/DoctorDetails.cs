c765e37d2069cd0b2506bfb1ec564cc40da48d95
Rajnesh Durai R T <127297822+Rajnesh-kanini@users.noreply.github.com>
2023-06-30 17:34:55 +0530

    Add files via upload

--- BackEnd/Models/DoctorDetails.cs
+++ BackEnd/Models/DoctorDetails.cs
@@ -12,6 +12,11 @@ namespace BigBangAngular30thJune.Models
         public string? DoctorName { get; set; }
         public string? Specialization { get; set; }
         public int YearsOfExperience { get; set; }
+        public string? EducationLevel { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? AlternatePhoneNumber { get; set; }
+        public string? EmailId { get; set; }
+        public string? Location { get; set; }
         public ICollection<Appointment>? Appointments { get; set; }
     }
 }
