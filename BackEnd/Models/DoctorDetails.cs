081e717625345a2f56e59c009abf569a8cce7004
Rajnesh Durai R T <127297822+Rajnesh-kanini@users.noreply.github.com>
2023-06-30 15:46:20 +0530

    Add files via upload

---
+++ BackEnd/Models/DoctorDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
+
+namespace BigBangAngular30thJune.Models
+{
+    public class DoctorDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string? DoctorName { get; set; }
+        public string? Specialization { get; set; }
+        public int YearsOfExperience { get; set; }
+        public ICollection<Appointment>? Appointments { get; set; }
+    }
+}
